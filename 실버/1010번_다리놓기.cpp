ac4908a7b2487cc321cc0e838a593a69f2b48107
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-07-22 11:31:10 +0900

    silver 5

--- 실버/1010번_다리놓기.cpp
+++ 실버/1010번_다리놓기.cpp
@@ -6,8 +6,8 @@ int main() {
 	cin >> T;
 	while (T--) {
 		cin >> N >> M;
-		unsigned long long int num = 1, den = 1; //최대 29!/15!의 연산을 하므로 큰 수를 다루는 정수형
-		if(N>M-N) //수가 너무 커지지 않도록 조합의 성질 이용
+		unsigned long long int num = 1, den = 1; 	// 최대 29!/15!의 연산을 하므로 큰 수를 다루는 정수형
+		if(N>M-N) 					// 수가 너무 커지지 않도록 조합의 성질 이용(nCm==nCn-m)
 			N=M-N;
 		for (int i = 0; i < N; i++) {
 			num = num * (M - i); //M!/(M-N)!
