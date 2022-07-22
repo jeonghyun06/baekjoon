8f71a2e75f7ea182bc2b2e79c170d43ea90444cf
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-07-22 11:18:57 +0900

    gold 5

--- 골드/2447번_별 찍기-10.cpp
+++ 골드/2447번_별 찍기-10.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int recursion(int N, int M) {
-	if (N % 3 == 1 && M % 3 == 1) //3X3기준 (1, 1) 지점에서 출력이 없어야 함
+	if (N % 3 == 1 && M % 3 == 1) // 3X3기준 (1, 1) 지점에서 출력이 없어야 함
 		return 1;
 	else if (N / 3 && M / 3) { // 몫이 있다면 나눠서 더 작은 범위일 때 (1, 1) 지점 확인
 		return recursion(N / 3, M / 3);
 	}
-	else //가장 작게 나누어도 (1, 1)인 부분이 없으면 * 출력
+	else // 가장 작게 나누어도 (1, 1)인 부분이 없으면 * 출력
 		return 0;
 }
 
