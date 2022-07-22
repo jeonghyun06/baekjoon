b4efeace3e04af6ccbff9f55d52be257ddd8df42
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-07-22 11:29:47 +0900

    silver 4

--- 실버/1065번_한수.cpp
+++ 실버/1065번_한수.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int main() {
 	int N, h, t, o, cnt;
 	cin>>N;
-	if(N<100)
+	if(N<100) //두자리 수는 무조건 등차수열
 		cout<<N;
 	else{
-		for(cnt=0;N>99;N--){
+		for(cnt=0;N>99;N--){ //100이상이면 각 자리수 등차확인
 			h = N/100;
 			t = (N%100)/10;
 			o = (N%100)%10;
-			if(h-t==t-o)
+			if(h-t==t-o) //등차확인
 				cnt++;
 		}
-		cout<<99+cnt;
+		cout<<99+cnt; //100이하의 한수 99개
 	}
 	return 0;
 }
