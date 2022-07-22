df66613c36ec66c670b901f44d9649023946fbeb
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-07-22 11:35:34 +0900

    silver 4

--- 실버/2839번_설탕배달.cpp
+++ 실버/2839번_설탕배달.cpp
@@ -9,16 +9,16 @@ int main() {
 	int result=-1;
 	cin>>N;
 	int a = N%5;
-	if(a){ //5kg 뭉치만으로 배달이 가능한지 판단
-		while(a<=N){ //그렇지 않으면 5씩 더하면서 3으로 나누어 떨어지는 경우 확인
-			if(a%3==0){ // 나누어 떨어지면 -> 최소경우
+	if(a){						//5kg 뭉치만으로 배달이 가능한지 판단
+		while(a<=N){ 				//그렇지 않으면 5씩 더하면서 3으로 나누어 떨어지는 경우 확인
+			if(a%3==0){ 			// 나누어 떨어지면 -> 최소경우
 				result=a/3+(N-a)/5;
 				break;
 			}
 			a=a+5;
 		}
 	}
-	else //가능하면
+	else
 		result = N/5;
 	
 	cout<<result;
