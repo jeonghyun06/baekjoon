67a7771a235ab9119e59ae6caaa86b66c314d214
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-08-10 01:31:45 +0900

    silver 2
    풀이를 떠올리지 못해서 질문에 들어갔다가 이항계수를 합을 통해 구할 수 있음을 깨닫고 해당 방법으로 풀이 + 동적 계획법을 사용하지 않으면 시간초과-> 동적계획법
    
    나중에 다시 풀어볼 것 요망.

---
+++ 11051번_이항계수2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int bi[1000][500] = { 0, };   //다이나믹 프로그래밍
+
+int binomal(int n, int r) {   //이항계수(파스칼의 삼각형) => nCr = n-1Cr + n-1Cr-1
+	if (bi[n][r])
+		return bi[n][r];
+	if (r == 0 or r == n)
+		return bi[n][r] = 1;
+	return bi[n][r] = (binomal(n - 1, r) + binomal(n - 1, r - 1)) % 10007;  //mod 연산에서 합차곱이 성립(나누기는 성립하지 않음)
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, K;
+	cin >> N >> K;
+
+	K = K > N - K ? N - K : K;
+	cout << binomal(N, K);
+
+	return 0;
+}
