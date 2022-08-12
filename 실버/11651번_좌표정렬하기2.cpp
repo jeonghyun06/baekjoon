48fb79acc979e6ecd0846ea79b72eefc4658ffb8
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-08-12 12:59:27 +0900

    silver 5

---
+++ 실버/11651번_좌표정렬하기2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Point {
+public:
+	int x, y;
+};
+
+bool compare(Point fir, Point sec) {
+	if (fir.y == sec.y) {
+		return fir.x < sec.x;
+	}
+	return fir.y < sec.y;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, k;
+	cin >> N;
+
+	Point* points = new Point[N];
+	for (int i = 0; i < N; i++) {
+		cin >> points[i].x >> points[i].y;
+	}
+	sort(points, points + N, compare);
+
+	for (int i = 0; i < N; i++) {
+		cout << points[i].x << ' ' << points[i].y << '\n';
+	}
+	return 0;
+}
