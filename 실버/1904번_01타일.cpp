679723b1bab4b0eb2bbd04f00e87204c7cb32c50
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-08-19 11:38:36 +0900

    silver 3
    주석부분은 처음 코드로 문제를 맞추긴 했지만 배열의 크기가 N과 무관하게 커서 메모리 소모가 많고 재귀로 구현하여 시간이 오래 걸렸다.
    그래서 재귀 말고 반복문으로 main함수 내부에 구현하여 시간을 단축하고 그 크기도 N+1의 크기로 제한해 메모리를 적절하게 사용하도록 하였다.

--- 실버/1904번_01타일.cpp
+++ 실버/1904번_01타일.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int comb[1000001] = { 0, };
+// int comb[1000001] = { 0, };
 
-int cal(int n) {  //피보나치수열
-	if (n == 1 or n == 0)
-		return comb[n] = 1;
-	else if (comb[n])
-		return comb[n];
-	else
-		return comb[n] = (cal(n - 1) + cal(n - 2)) % 15746;
-}
+// int cal(int n) {  //피보나치수열
+// 	if (n == 1 or n == 0)
+// 		return comb[n] = 1;
+// 	else if (comb[n])
+// 		return comb[n];
+// 	else
+// 		return comb[n] = (cal(n - 1) + cal(n - 2)) % 15746;
+// }
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -19,7 +19,16 @@ int main() {
 
 	int N;
 	cin >> N;
-	cout << cal(N);
+// 	cout << cal(N);
+	
+	int* comb = new int[N+1];
+	for(int i=0;i<=N;i++){
+		if (i == 1 or i == 0)
+			comb[i] = 1;
+		else
+			comb[i] = (comb[i - 1] + comb[i - 2]) % 15746;
+	}
+	cout << comb[N];
 
 	return 0;
 }
