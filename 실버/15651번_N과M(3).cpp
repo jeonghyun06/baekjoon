3fab2f82adf2c006bbc6b72a57d430a5f96b68fe
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-09-07 10:38:29 +0900

    silver 3

---
+++ 실버/15651번_N과M(3).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> num;
+
+void backtracking(int cnt){
+	if(cnt==M){
+		for(int i=0;i<M;i++)
+			cout<<num[i]<<' ';
+		cout<<'\n';
+		return;
+	}
+	for(int i=0;i<N;i++){
+		num.push_back(i+1);
+		backtracking(cnt+1);
+		num.pop_back();
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); 
+	cout.tie(0);
+	
+	cin>>N>>M;
+	backtracking(0);
+	
+	return 0;
+}
